d7ceb3ecf40f845b485bc05a8a451269d50471e1
77xt <77xt@proton.me>
2026-02-22 19:05:29 +0100

    add CommitHashRaw string

--- include/meta.h
+++ include/meta.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#define KINETIC_STR_ALT(value) #value
+
+#define KINETIC_STR(value) KINETIC_STR_ALT(value)
+
 #define KINETIC_GETTER(field, name) \
   auto get_##name() const -> decltype(field) { return field; }
 
