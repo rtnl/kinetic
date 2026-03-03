96100a32bd6593ae7960299e5d0fc5ca24a0fc81
77xt <77xt@proton.me>
2026-03-04 00:21:38 +0100

    implement ByteBuffer read and read_string

--- include/buffer.h
+++ include/buffer.h
@@ -37,6 +37,37 @@ public:
   size_t write(const std::string & source) {
     return write(source.c_str(), strlen(source.c_str()));
   }
+
+  size_t read(u8 * destination, const size_t len) const {
+    for (size_t x = 0; x < len; x++) {
+      destination[x] = _inner[x];
+    }
+
+    return len;
+  }
+
+  size_t read_string(std::string & destination) const {
+    bool flag_null_terminated = false;
+    size_t index_end = 0;
+
+    for (size_t x = 0; x < size(); x++) {
+      if (_inner[x] == 0x00) {
+        flag_null_terminated = true;
+        index_end = x;
+        break;
+      }
+    }
+
+    if (!flag_null_terminated) {
+      return 0;
+    }
+
+    const std::string s((char *) raw(), index_end);
+
+    destination.append(s);
+
+    return index_end;
+  }
 };
 
 }
