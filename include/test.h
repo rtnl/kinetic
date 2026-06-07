4621173a4b19648299664e489046daf883e48c3e
77xt <77xt@proton.me>
2026-06-07 18:56:53 +0200

    update test.h

--- include/test.h
+++ include/test.h
@@ -1,12 +1,56 @@
 #pragma once
 
+#include <./meta.h>
 #include <./type.h>
+#include <./log.h>
 #include <./program.h>
+#include <stdexcept>
 
 namespace kinetic {
 
-class TestContext {
+enum class TestContextState {
+  Fail,
+  Pass,
+};
 
+inline std::string Display(const TestContextState state) {
+  switch (state) {
+    case TestContextState::Fail: return "fail";
+    case TestContextState::Pass: return "pass";
+    default:
+      throw std::runtime_error("unimplemented");
+  }
+}
+
+class TestContext {
+private:
+  std::string _name;
+
+  TestContextState _state;
+
+public:
+  TestContext(const std::string & name)
+    : _name(name)
+    , _state(TestContextState::Fail)
+  {}
+
+  ~TestContext() {
+    _LOG_DEFAULT.Test(_name + " - " + Display(_state));
+
+    if (get_state() != TestContextState::Pass) {
+      ::exit(1);
+    }
+  }
+
+  std::string get_name() const {
+    return _name;
+  }
+
+  KINETIC_GETTER(_state, state)
+
+  void ok() {
+    _state = TestContextState::Pass;
+  }
 };
 
 class TestProgram : kinetic::Program<u8> {
@@ -21,7 +65,7 @@ public:
 
 }
 
-#define __KINETIC_TEST_CASE_IMPL_FUNC(_NAME) void _NAME(const kinetic::TestContext & ctx)
+#define __KINETIC_TEST_CASE_IMPL_FUNC(_NAME) void _NAME(kinetic::TestContext & test_ctx)
 
 #ifdef KINETIC_TEST
 #define __KINETIC_TEST_CASE_IMPL_INSTANCE(_NAME) static _NAME _NAME_##_I;
@@ -29,17 +73,17 @@ public:
 #define __KINETIC_TEST_CASE_IMPL_INSTANCE(_NAME) ;
 #endif
 
-#define __KINETIC_TEST_CASE_IMPL_CLASS(_NAME, _FUNC) \
-class _NAME { public: _NAME() { _FUNC(kinetic::TestContext()); } }; \
-__KINETIC_TEST_CASE_IMPL_INSTANCE(_NAME)
+#define __KINETIC_TEST_CASE_IMPL_CLASS(_NAME, _KEY_C, _FUNC) \
+class _KEY_C { public: _KEY_C() { kinetic::TestContext test_ctx(#_NAME); _FUNC(test_ctx); } }; \
+__KINETIC_TEST_CASE_IMPL_INSTANCE(_KEY_C)
 
 #define __KINETIC_TEST_CASE_IMPL(_KEY, _NAME)        \
 __KINETIC_TEST_CASE_IMPL_FUNC(_KEY##_F);            \
-__KINETIC_TEST_CASE_IMPL_CLASS(_KEY##_C, _KEY##_F) \
+__KINETIC_TEST_CASE_IMPL_CLASS(_NAME, _KEY##_C, _KEY##_F) \
 __KINETIC_TEST_CASE_IMPL_FUNC(_KEY##_F)
 
 #ifdef KINETIC_TEST
-#define KINETIC_TEST_CASE(_NAME) __KINETIC_TEST_CASE_IMPL(__KINETIC_TEST_CASE_##counter, _NAME)
+#define KINETIC_TEST_CASE(_NAME) __KINETIC_TEST_CASE_IMPL(__KINETIC_TEST_CASE_##_NAME, _NAME)
 #else
-#define KINETIC_TEST_CASE(_NAME) template<typename T = void> void kinetic_test_skip_##__COUNTER__()
+#define KINETIC_TEST_CASE(_NAME) template<typename T = void> void kinetic_test_skip_##_NAME(kinetic::TestContext & test_ctx)
 #endif
